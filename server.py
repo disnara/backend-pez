67a73be17b79a36332ebd1df7e015d759b1d83a2
disnara <143056571+disnara@users.noreply.github.com>
2026-01-19 17:24:57 +0530

    Update server.py

--- server.py
+++ server.py
@@ -234,7 +234,7 @@ async def get_menace_leaderboard(
 # Set to end at midnight (12:00 AM) UTC for each competition period
 LEADERBOARD_END_TIMES = {
     "metaspins": datetime(2026, 2, 1, 0, 0, 0, tzinfo=timezone.utc),    # End: 12:00 AM, 1 February 2026 (UTC)
-    "menace": datetime(2026, 1, 23, 0, 0, 0, tzinfo=timezone.utc),      # End: 12:00 AM, 23 January 2026 (UTC)
+    "menace": datetime(2026, 1, 24, 0, 0, 0, tzinfo=timezone.utc),      # End: 12:00 AM, 23 January 2026 (UTC)
     "winovo": datetime(2026, 1, 21, 0, 0, 0, tzinfo=timezone.utc),      # End: 12:00 AM, 20 January 2026 (UTC)
 }
 
